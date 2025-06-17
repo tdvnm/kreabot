d22a6ce10b1d1fe96c2e587f592d6ef253609414
tdvnm <shubhrogupta117@gmail.com>
2025-06-18 02:31:20 +0530

    Update +page.svelte

--- src/routes/set/+page.svelte
+++ src/routes/set/+page.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
-    import { auth } from '$lib/firebase';
-    import { signOut } from 'firebase/auth';
+	import { auth } from '$lib/firebase';
+	import { signOut } from 'firebase/auth';
 
-    async function handleSignOut() {
-        await signOut(auth);
-    }
+	async function handleSignOut() {
+		await signOut(auth);
+	}
+	let darkMode = false;
+
+	function toggleDarkMode() {
+		darkMode = !darkMode;
+		document.documentElement.setAttribute('data-theme', darkMode ? 'dark' : 'light');
+	}
 </script>
 
 <h1>Settings</h1>
 <button on:click={handleSignOut}>Sign out</button>
+
+<button on:click={toggleDarkMode}>
+	{darkMode ? 'Light Mode' : 'Dark Mode'}
+</button>
